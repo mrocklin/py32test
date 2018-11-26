a6b4c4545af6cd148f40a09441832fe3f2b3fef2
Matthew Rocklin <mrocklin@gmail.com>
2018-11-26 08:52:48 -0500

    Move cythonize code into conditional

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
-from setuptools import setup, find_packages
 import os
-from Cython.Build import cythonize
+from setuptools import setup, find_packages
+import sys
+
+if sys.version_info[0] == 2:
+    from Cython.Build import cythonize
+    kwargs = {'ext_modules': cythonize(os.path.join("py32test", "*.py"),
+                                       language_level='3')}
+else:
+    kwargs = {}
 
 setup(
     name='py32test',
-    version='0.0.1',
-    ext_modules = cythonize(os.path.join("py32test", "*.py"),
-                            language_level='3'),
+    version='1.0.0',
     packages=find_packages(),
+    **kwargs
 )
 
